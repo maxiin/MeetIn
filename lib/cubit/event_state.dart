af9746c044c6a105a93ffd3168cdff8946755e3d
Max <raranmariano@hotmail.com>
2020-08-28 15:39:42 -0300

    event states

--- lib/cubit/event_state.dart
+++ lib/cubit/event_state.dart
@@ -1,6 +1,20 @@
 part of 'event_cubit.dart';
 
 @immutable
-abstract class EventState {}
+abstract class EventState {
+  final events;
+  EventState(this.events);
+}
 
-class EventInitial extends EventState {}
+class EventInitial extends EventState {
+  EventInitial() : super([]);
+}
+
+class EventLoading extends EventState {
+  EventLoading() : super([]);
+}
+
+class EventLoaded extends EventState {
+  final List<Event> events;
+  EventLoaded(this.events) : super(events);
+}
